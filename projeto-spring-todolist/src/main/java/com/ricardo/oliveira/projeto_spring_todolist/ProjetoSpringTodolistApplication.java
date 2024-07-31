01e5b09d88b6f96be24825a051945d7716b89fc0
Ricardo Firme de Oliveira <rick.firmeoliveira2@gmail.com>
2024-07-31 17:58:22 -0300

    initial commit

---
+++ projeto-spring-todolist/src/main/java/com/ricardo/oliveira/projeto_spring_todolist/ProjetoSpringTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.oliveira.projeto_spring_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringTodolistApplication.class, args);
+	}
+
+}
---
+++ projeto-spring-todolist/src/test/java/com/ricardo/oliveira/projeto_spring_todolist/ProjetoSpringTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.oliveira.projeto_spring_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
